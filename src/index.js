e99d3138f463fdd619e8775869d109910da1904f
Alfred Skaria <alfiskaria@gmail.com>
2020-05-07 23:38:44 +0530

    Implemented the axios interceptors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.interceptors.request.use(
+  req => {
+    console.log(req);
+    //Edit the request config is possible
+    return req;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  res => {
+    console.log(res);
+    //Edit the reponse config is possible
+    return res;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
