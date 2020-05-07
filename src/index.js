c2988985d344a26c6ddfe7cfc75fc1951e6025b2
Alfred Skaria <alfiskaria@gmail.com>
2020-05-07 23:54:12 +0530

    Implemented the instance creation of axios.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/';
-axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 
