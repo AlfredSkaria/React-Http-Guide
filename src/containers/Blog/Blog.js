b1adc7e7dbc8feed3cb00ffb405136b4637d64c0
Alfred Skaria <alfiskaria@gmail.com>
2020-05-11 13:21:20 +0530

    Added navigation items to the home page for building the Router functionality.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -41,6 +41,7 @@ class Blog extends Component {
 
     render () {
 
+        
         let posts = <p style={{textAlign: 'center'}}>Something went wrong..!!</p>
         if (!this.state.error){
             posts = this.state.posts.map(
@@ -55,7 +56,15 @@ class Blog extends Component {
         }
 
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
